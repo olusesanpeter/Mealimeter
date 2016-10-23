346365cb77e0348203a11fec494a8342ed74bf9e
Olusesan Peter <olusesanpeter@gmail.com>
2016-10-23 13:07:48 +0100

    Initial Push

---
+++ angular-scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mealimeterApp
+ * @description
+ * # mealimeterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mealimeterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/pre-order.html',
+        controller: 'Pre-orderCtrl',
+        controllerAs: 'main'
+      })
+      .when('/top-up', {
+        templateUrl: 'views/top-up.html',
+        controller: 'Top-upCtrl',
+        controllerAs: 'about'
+      })
+      .when('/account-settings', {
+        templateUrl: 'views/account-settings.html',
+        controller: 'Account-settingsCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ angular-scripts/controllers/pre-order.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mealimeterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mealimeterApp
+ */
+angular.module('mealimeterApp')
+  .controller('Pre-orderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
