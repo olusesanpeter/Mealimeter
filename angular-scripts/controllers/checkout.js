471f2a7739584dd0fe85b8449a8e86755824caa8
Olusesan Peter <olusesanpeter@gmail.com>
2016-10-30 15:40:26 +0100

    added checkout page

---
+++ angular-scripts/controllers/checkout.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mealimeterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mealimeterApp
+ */
+angular.module('mealimeterApp')
+  .controller('CheckoutCtrl', function () {
+    this.awesomeThings = [
+      ''
+    ];
+  });
